f44541fe3ca855525efb3b2d4e32f2b094bf717b
Carsten Rohde <carsten.rohde@obi.de>
2022-01-24 15:50:52 +0100

    FIX: computation of matching gavs fixed ADD: tests for new predicates and functions

--- plugin/src/main/java/com/itemis/maven/plugins/unleash/util/functions/GAVPatternToCoordinates.java
+++ plugin/src/main/java/com/itemis/maven/plugins/unleash/util/functions/GAVPatternToCoordinates.java
@@ -6,14 +6,20 @@ package com.itemis.maven.plugins.unleash.util.functions;
 import com.google.common.base.Function;
 import com.itemis.maven.aether.ArtifactCoordinates;
 
-
 public class GAVPatternToCoordinates implements Function<String, ArtifactCoordinates> {
 
-    @Override
-    public ArtifactCoordinates apply(String gav) {
-        String[] tokens = gav.split(":");
-        return new ArtifactCoordinates(tokens[0], tokens[1], tokens[2]);
-    }
+	@Override
+	public ArtifactCoordinates apply(String gav) {
+		String[] tokens = gav.split(":");
+		switch (tokens.length) {
+		case 1:
+			return new ArtifactCoordinates(tokens[0], null, null);
+		case 2:
+			return new ArtifactCoordinates(tokens[0], tokens[1], null);
+		case 3:
+		default:
+			return new ArtifactCoordinates(tokens[0], tokens[1], tokens[2]);
+		}
+	}
 
 }
-
--- plugin/src/main/java/com/itemis/maven/plugins/unleash/util/predicates/IsMatchingCoordinates.java
+++ plugin/src/main/java/com/itemis/maven/plugins/unleash/util/predicates/IsMatchingCoordinates.java
@@ -14,50 +14,47 @@ import com.google.common.collect.Sets;
 import com.itemis.maven.aether.ArtifactCoordinates;
 import com.itemis.maven.plugins.unleash.util.functions.GAVPatternToCoordinates;
 
-
 public class IsMatchingCoordinates implements Predicate<ArtifactCoordinates> {
 
-    private final Set<ArtifactCoordinates> referencePatterns;
+	private final Set<ArtifactCoordinates> referencePatterns;
 
+	public IsMatchingCoordinates(List<String> someAllowedSnapshots) {
+		List<String> allowedSnapshots = someAllowedSnapshots != null ? someAllowedSnapshots : new ArrayList<>();
 
-    public IsMatchingCoordinates(List<String> someAllowedSnapshots) {
-        List<String> allowedSnapshots = someAllowedSnapshots != null ? someAllowedSnapshots : new ArrayList<>();
+		referencePatterns = Sets.newHashSet(Collections2.transform(allowedSnapshots, new GAVPatternToCoordinates()));
+	}
 
-        referencePatterns = Sets.newHashSet(Collections2.transform(allowedSnapshots, new GAVPatternToCoordinates()));
-    }
+	@Override
+	public boolean apply(ArtifactCoordinates coordinates) {
+		for (ArtifactCoordinates referencePattern : referencePatterns) {
+			if (matchesPattern(coordinates, referencePattern)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    @Override
-    public boolean apply(ArtifactCoordinates coordinates) {
-        for (ArtifactCoordinates referencePattern : referencePatterns) {
-            if (matchesPattern(coordinates, referencePattern)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	private boolean matchesPattern(ArtifactCoordinates aCoordinates, ArtifactCoordinates aReferencePattern) {
+		String tReferenceGroupId = aReferencePattern.getGroupId();
+		String tGroupId = aCoordinates.getGroupId();
 
-    private boolean matchesPattern(ArtifactCoordinates aCoordinates, ArtifactCoordinates aReferencePattern) {
-        String tReferenceGroupId = aReferencePattern.getGroupId();
-        String tGroupId = aCoordinates.getGroupId();
+		String tReferenceArtifactId = aReferencePattern.getArtifactId();
+		String tArtifactId = aCoordinates.getArtifactId();
+		String tReferenceVersion = aReferencePattern.getVersion();
+		String tVersion = aCoordinates.getVersion();
 
-        String tReferenceArtifactId = aReferencePattern.getArtifactId();
-        String tArtifactId = aCoordinates.getArtifactId();
-        String tReferenceVersion = aReferencePattern.getVersion();
-        String tVersion = aCoordinates.getVersion();
+		boolean tGroupIdMatches = wildcardOrLiteralMatch(tReferenceGroupId, tGroupId);
+		boolean tArtifactIdMatches = tReferenceArtifactId == null
+				|| wildcardOrLiteralMatch(tReferenceArtifactId, tArtifactId);
+		boolean tVersionMatches = tVersion == null || wildcardOrLiteralMatch(tReferenceVersion, tVersion);
 
+		return tGroupIdMatches && tArtifactIdMatches && tVersionMatches;
+	}
 
-        boolean tGroupIdMatches = wildcardOrLiteralMatch(tReferenceGroupId, tGroupId);
-        boolean tArtifactIdMatches =
-                tReferenceArtifactId == null || wildcardOrLiteralMatch(tReferenceArtifactId, tArtifactId);
-        boolean tVersionMatches = tVersion == null || wildcardOrLiteralMatch(tReferenceVersion, tVersion);
-
-        return tGroupIdMatches && tArtifactIdMatches && tVersionMatches;
-    }
-
-    private boolean wildcardOrLiteralMatch(String tLeft, String tRight) {
-        return tLeft.equals("*") || tRight.equals("*") || Objects.equal(tLeft, tRight);
-    }
-
+	private boolean wildcardOrLiteralMatch(String tLeft, String tRight) {
+		boolean atLeastOneIsNull = tLeft == null || tRight == null;
+		return atLeastOneIsNull || Objects.equal(tLeft, tRight) || Objects.equal(tLeft, "*")
+				|| Objects.equal(tRight, "*");
+	}
 
 }
-
